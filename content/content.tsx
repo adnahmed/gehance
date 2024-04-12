46601e84ec30226f4e9efec4f1f12a813af627cd
Sulman Ahmed Khan <slmkhanahmed@gmail.com>
2024-04-12 15:11:36 +0500

    add before star list the search bar

--- content/content.tsx
+++ content/content.tsx
@@ -7,12 +7,23 @@ setTimeout(initial, 1000)
 
 function initial() {
   // Create a new div element and append it to the document's body
-  const rootDiv = document.createElement('div')
-  rootDiv.id = 'extension-root'
-  document.body.appendChild(rootDiv)
+  // document.body.appendChild(rootDiv)
 
   // Use `createRoot` to create a root, then render the <App /> component
   // Note that `createRoot` takes the container DOM node, not the React element
-  const root = ReactDOM.createRoot(rootDiv)
+  const rootDiv = document.createElement('div');
+  const root = ReactDOM.createRoot(rootDiv);
+ 
+  // m1:
+  // const beforeSearch = document.querySelector('adadad');
+  // m2: 
+  const xeval = new XPathEvaluator();
+  const res = xeval.evaluate(`//\*[@id="user-list-repositories"]`, document.body);
+  const beforeSearch = res.iterateNext()
+  const beforeSearchParent = beforeSearch?.parentNode;
+// beforeSearch?.insertBefore(rootDiv);
+if (beforeSearchParent)
+beforeSearchParent.insertBefore(rootDiv, beforeSearch);
+
   root.render(<ContentApp />)
 }
